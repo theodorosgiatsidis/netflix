b6db1ba10950b4d78e9f2a8855f7457948c6d2f4
Thodoros Giatsidis <46629765+theodorosgiatsidis@users.noreply.github.com>
2022-01-24 16:41:36 +0200

    express features

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import "./App.scss";
+// import Login from "./pages/login/Login";
+// import Register from "./pages/register/Register";
+import Home from "../../client/src/pages/home/Home";
+// import Watch from "./pages/watch/Watch";
+
+function App() {
+  return (
+    <div className="App">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
